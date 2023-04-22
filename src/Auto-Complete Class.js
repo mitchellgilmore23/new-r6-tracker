106c8eb630e9e0d9ead48ea72e0b51ff849bf3de
mitchellgilmore23 <117942362+mitchellgilmore23@users.noreply.github.com>
2023-04-21 20:50:56 -0500

    f

--- src/Auto-Complete Class.js
+++ src/Auto-Complete Class.js
@@ -1,37 +1,135 @@
-export default class {
+
+
+const $ = require('jquery');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const proxy = 'https://tracker-proxy.herokuapp.com/'
+export class autoComplete {
 	constructor(lookupName, currentPlayerCol, lookupPlatform) {
     this.lookupName = lookupName;
-    this.currentPlayerCol = currentPlayerCol;
-    this.lookupPlatform = lookupPlatform;
-  }
-	async fetch(){
-		console.log(this.lookupName, this.currentPlayerCol, this.lookupPlatform)
-		let url = `https://tracker-proxy.herokuapp.com/https://r6.tracker.network/r6/autocomplete?name=${this.lookupName}&platform=${this.lookupPlatform}`;
-		// var length = $(`#p${player}Input`).width();
-		// $(`#p${player}DropdownClass`).css("width", length * 1.08 + "px");
-		// if (name.length < 1) {
-		//   $(`#p${player}DropdownClass`).removeClass("show");
-		//   return;
-		const a = fetch(url).then(r => r.json());
-		this.insertResults(await a);
+		this.currentPlayerCol = currentPlayerCol;
+		this.lookupPlatform = lookupPlatform;
+		this.fetch()
 	}
-	async insertResults(a){
-		$(`#p${player}DropdownClass > button`).remove();
-		b.forEach((item, index, array) => {
-			let newHtml = `<button class="list-group-item d-flex justify-content-between align-items-center rounded-2" 
-			role="button" 
-			attr="player${player}AutoData" 
-			tabindex="${index + 1}">${item.name}<span class="badge rounded-pill">
+	async fetch(){
+		let url = `https://tracker-proxy.herokuapp.com/https://r6.tracker.network/r6/autocomplete?name=${this.lookupName}&platform=${this.lookupPlatform}`;
+		const array = fetch(url).then(r => r.json());
+		this.insertResults(await array);
+	}
+	async insertResults(array){
+		this.clearResults();
+		let ulDiv = $(`[player=${this.currentPlayerCol}] ul`)
+		if (this.lookupName.length < 1) array = [];
+		array.forEach((item, index, array) => {
+			let newHtml = `
+			<button 
+				class="list-group-item d-flex justify-content-between align-items-center rounded-2 w-100" 
+				tabindex="${index + 1}" 
+				attr='autocomplete-dropdown-items-to-delete'
+				onclick="dropdownClicked($(this),${this.currentPlayerCol})"
+				>${item.name}<span class="badge rounded-pill"> 
 					Level ${item.cl}
 				</span>
 			</button>
 			`;
-			$(`[dropdown='${player}]'`).before(newHtml);
+			ulDiv.append(newHtml)
 		});
-		$(`#p${player}DropdownClass`).removeAttr("hidden");
-    console.log(a)
 	}
-	clear(){
+	clearResults(){
+		$(`[attr='autocomplete-dropdown-items-to-delete']`).remove()
+	}
+}
 
+export class getRankedData {
+  constructor(lookupName,currentPlayerCol,lookupPlatform) {
+    this.lookupName = lookupName;
+		this.currentPlayerCol = currentPlayerCol;
+		this.lookupPlatform = lookupPlatform;
+		this.fetchMain(this.lookupName,this.lookupPlatform)
+		this.fetchMatchHistory(this.lookupName,this.lookupPlatform)
+		this.fetchSeasonHistory(this.lookupName,this.lookupPlatform)
+
+  }
+  async fetchMain(lookupName,lookupPlatform) {
+		var newArr = [];
+		if (lookupName.length < 1 || lookupPlatform != 'xbox' && lookupPlatform != 'psn') return;
+		await axios({
+			timeout: 8000,
+			method: 'get',
+      headers: {
+        'accept-language': 'en-US,en;q=0.8,et;q=0.6',
+				"cache-control": "max-age=0"
+    	},
+			url: `${proxy}https://r6.tracker.network/profile/${lookupPlatform}/${lookupName}`,
+		})
+		.then(response => {
+			var axiosData = cheerio.load(response.data)
+			axiosData('#profile .trn-card').each((i,arr) => newArr.push($(arr).text().replace(/(\n){1,}/gm,'\n').trim().split('\n')))
+			newArr.forEach((arr,arrI) => arr.forEach((val,valI) =>  {
+				newArr[arrI][valI] = val.replace(/^ | $/g,'')
+				if (val.length < 1 || val == ' ' || val == '') newArr[arrI].splice(valI,1)
+			}))
+			this.mainArray = newArr;
+		})
+		.catch(error => console.log(error))
+	}
+	async fetchMatchHistory(lookupName,lookupPlatform) {
+		var newArr = [];
+		if (lookupName.length < 1 || lookupPlatform != 'xbox' && lookupPlatform != 'psn') return;
+		await axios({
+			timeout: 8000,
+			method: 'get',
+      headers: {
+        'accept-language': 'en-US,en;q=0.8,et;q=0.6',
+				"cache-control": "max-age=0"
+    	},
+			url: `${proxy}https://r6.tracker.network/profile/${lookupPlatform}/${lookupName}/mmr-history`,
+		})
+		.then(response => {
+			var axiosData = cheerio.load(response.data)
+			axiosData('.trn-table__row').each((i,arr) => newArr.push($(arr).text().replace(/(\n){1,}/gm,'\n').trim().split('\n')))
+				// if (i < 21) {
+				// 	if (i > 0 && $(this)[0].children[7].children[1] !== undefined) {
+				// 		a["matchHistory"][i].push($(this)[0].children[7].children[1].attribs["src"]);
+				// 		a["matchHistory"][i].push($(this)[0].children[1].children[1].attribs["v-human-time"]);
+				// 		a["matchHistory"][i][7] = a["matchHistory"][i][7].replaceAll(`'`, ``);
+				// 		a["matchHistory"][i].push(humanTime(a["matchHistory"][i][7]));
+				// 	}
+				// }
+
+			
+			newArr.forEach((arr,arrI) => arr.forEach((val,valI) =>  {
+				newArr[arrI][valI] = val.replace(/^ | $/g,'')
+				if (val.length < 1 || val == ' ' || val == '') newArr[arrI].splice(valI,1)
+			}))
+			this.MatchHistory = newArr;
+			console.log(newArr)
+		})
+		.catch(error => console.log(error))
+	}
+	async fetchSeasonHistory(lookupName,lookupPlatform) {
+		var newArr = [];
+		if (lookupName.length < 1 || lookupPlatform != 'xbox' && lookupPlatform != 'psn') return;
+		await axios({
+			timeout: 8000,
+			method: 'get',
+      headers: {
+        'accept-language': 'en-US,en;q=0.8,et;q=0.6',
+				"cache-control": "max-age=0"
+    	},
+			url: `${proxy}https://r6.tracker.network/profile/${lookupPlatform}/${lookupName}/seasons`,
+		})
+		.then(response => {
+			var seasons = cheerio.load(response.data)
+			seasons('.trn-card').each((i,arr) => newArr.push($(arr).text().replace(/(\n){1,}/gm,'\n').trim().split('\n')))
+
+			newArr.forEach((arr,arrI) => arr.forEach((val,valI) =>  {
+				newArr[arrI][valI] = val.replace(/^ | $/g,'')
+				if (val.length < 1 || val == ' ' || val == '') newArr[arrI].splice(valI,1)
+			}))
+			this.SeasonHistory = newArr;
+			console.log(newArr)
+		})
+		.catch(error => console.log(error))
 	}
 }
--- src/common.js
+++ src/common.js
@@ -1,27 +1,241 @@
-const axios = require('axios')
-const moment = require('moment')
-const cheerio = require("cheerio");
-export const $ = require("jquery");
+const axios = require('axios'); const moment = require('moment');const cheerio = require("cheerio");
+export const $ = require("jquery"); window.$= $
 export const bootstrap = require("bootstrap");
 import rankImg from './rank-img-fetch.js'
 
-async function exec(url) {
-  const { data } = await axios({
-    method: "GET",
-    url: `https://tracker-proxy.herokuapp.com/` + url,
-    headers: {
-      "accept-language": "en-US,en;q=0.8,et;q=0.6",
-      "cache-control": "max-age=0",
-    },
-    timeout: 10000,
-  }).catch((error) => {
-    return error;
-  });
+export function loadPlayerCards(i) {
+  const data = `
+  <div class="btn-group w-100">
+    <button class="btn rounded-2 btn-outline-success my-2 w-50" type="button" attr="input-group-button-submit" tabindex="-1">Submit</button>
+    <button class="btn w-50 rounded-2 btn-outline-info my-2 w-50" type="button" attr="input-group-button-refresh" tabindex="-1" hidden>Refresh</button>
+  </div>
+  <div class="input-group mb-1 mt-1">
+    <input type="text" class="form-control" placeholder="Enter name.." attr="input-group-text" tabindex="-1" />
+    <button class="btn btn-outline-secondary active" type="button" attr="input-group-button-xbox" tabindex="-1" data-bs-title="Use the backquote key (~) to change platforms on the fly!">
+      XBOX
+    </button>
+    <button class="btn btn-outline-secondary" type="button" tabindex="-1" attr="input-group-button-ps">PS</button>
+  </div>
+
+	<ul class="list-group dropdown-menu p-0"></ul>
+
+	<div attr="notFound" class="container w-100 bg-warning p-2 my-3 rounded-3 opacity-50" hidden><p>Player Not Found..</p></div>
+
+	<div attr="notRanked" class="container w-100 bg-warning p-2 my-3 rounded-3 opacity-50" hidden><p>Player Not Ranked..</p></div>
+
+	<div attr="Spinner" class="spinner-border m-4 text-white" hidden style="width: 2.5rem; height: 2.5rem" role="status"></div>
+
+	<div class="card border-light lh-sm text-center mt-3 z-1">
+		<a class="card-body link-info fs-5 my-2" attr="card-header" target="_blank" tabindex="-1"></a>
+
+    <div class="accordion text-white">
+     <div class="accordion-item">
+      <h2 class="accordion-header">
+        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${i}1" aria-expanded="true" aria-controls="collapse${i}1" tabindex="-1">
+          <div class="hstack gap-3">
+            <img attr="accordion-title-rank-img" style="height: 46px" src="./media/UNRANKED.PNG" />
+            <p class="m-0" attr="accordion-title-url"></p>
+          </div>
+        </button>
+      </h2>
+      <div id="collapse${i}1" class="accordion-collapse collapse show" data-bs-parent="#accordion${i}">
+        <div class="accordion-body p-0">
+          <div class="card m-0 rounded-0" style="max-width: 740px; min-width: 100px">
+            <p class="fs-4 my-2 text-decoration-underline">Current</p>
+            <div class="row p-2 m-2 align-items-center">
+              <div class="col">
+                <p attr="accordion-card-1-kd" class="fw-bold mb-2 fs-5"></p>
+                <p attr="accordion-card-1-rank" class="mb-2 text-primary fs-5 text-nowrap"></p>
+                <p attr="accordion-card-1-mmr" class="mb-2 fs-5"></p>
+              </div>
+              <div class="col align-items-center">
+                <img src="./media/UNRANKED.PNG" attr="accordion-card-1-img" class="" style="width: 70px" alt="..." />
+              </div>
+            </div>
+            <p class="fs-4 my-2 text-decoration-underline">Seasonal Record</p>
+            <div class="row p-2 m-2 align-items-center">
+              <div class="col">
+                <p attr="accordion-card-1-max-rank" class="mb-2 text-primary fs-5 text-nowrap"></p>
+                <p attr="accordion-card-1-max-mmr" class="mb-2 fs-5"></p>
+              </div>
+              <div class="col">
+                <img attr="accordion-card-1-img" src="./media/UNRANKED.PNG" class="" style="width: 60px" alt="..." />
+              </div>
+            </div>
+            <div class="hstack m-2 p-2">
+              <div class="vstack">
+                <p class="card-text mb-0">Matches:</p>
+                <p attr="accordion-card-1-matches" class="card-text"></p>
+              </div>
+              <div class="vr"></div>
+              <div class="vstack">
+                <p class="card-text mb-0">Wins:</p>
+                <p attr="accordion-card-1-wins" class="card-text"></p>
+              </div>
+              <div class="vr"></div>
+              <div class="vstack">
+                <p class="card-text mb-0">Losses:</p>
+                <p attr="accordion-card-1-losses" class="card-text"></p>
+              </div>
+              <div class="vr"></div>
+              <div class="vstack">
+                <p class="card-text mb-0">Win %</p>
+                <p attr="accordion-card-1-win_" class="card-text"></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="accordion-item">
+      <h2 class="accordion-header">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${i}2" aria-expanded="false" aria-controls="collapse${i}2" tabindex="-1">
+          <div class="hstack gap-3">
+            <img id="p${i}card2HeaderImg" src="./media/UNRANKED.PNG" style="height: 46px" alt="..." />
+            <p class="m-0" attr="accordion-card-2-header-text"></p>
+          </div>
+        </button>
+      </h2>
+
+      <div id="collapse${i}2" class="accordion-collapse collapse" data-bs-parent="#accordion${i}">
+        <div class="accordion-body p-0">
+          <div class="card m-0 rounded-0" style="max-width: 740px; min-width: 100px">
+            <p class="fs-4 my-2 text-decoration-underline">Overall Ranked</p>
+            <div class="row p-2 m-2 align-items-center">
+              <div class="col">
+                <p class="mb-0 text-decoration-underline">Kills/Match:</p>
+                <p attr="accordion-card-2-kills-match" class="mb-3 fs-5"></p>
+
+                <p class="mb-0 text-decoration-underline">Kills/Min:</p>
+                <p attr="accordion-card-2-kills-min" class="mb-3 fs-5"></p>
+
+                <p class="mb-0 text-decoration-underline">Time Played:</p>
+                <p attr="accordion-card-2-time-played" class="fs-5"></p>
+              </div>
+              <div class="col align-items-center">
+                <p class="mb-0 text-decoration-underline">Average K/D:</p>
+                <p attr="accordion-card-2-average-kd" class="mb-3 fs-5"></p>
+
+                <p class="mb-0 text-decoration-underline">Total Kills:</p>
+                <p attr="accordion-card-2-total-kills" class="mb-3 fs-5"></p>
+
+                <p class="mb-0 text-decoration-underline">Total Deaths:</p>
+                <p attr="accordion-card-2-total-deaths" class="fs-5"></p>
+              </div>
+            </div>
+
+            <p class="fs-4 my-2 text-decoration-underline">Personal Record</p>
+
+            <div class="row p-2 m-2 align-items-center">
+              <div class="col">
+                <p attr="accordion-card-2-record-season" class="mb-2"></p>
+
+                <p attr="accordion-card-2-record-rank" class="mb-2 text-primary"></p>
+                <p attr="accordion-card-2-record-mmr" class="mb-2"></p>
+              </div>
+              <div class="col align-items-center">
+                <img src="" attr="accordion-card-2-record-img" class="" style="width: 80px" alt="..." />
+              </div>
+            </div>
+
+            <div class="hstack m-2 p-2">
+              <div class="vstack">
+                <p class="card-text mb-0">Matches:</p>
+                <p attr="accordion-card-2-matches" class="card-text"></p>
+              </div>
+              <div class="vr"></div>
+              <div class="vstack">
+                <p class="card-text mb-0">Wins:</p>
+                <p attr="accordion-card-2-wins" class="card-text"></p>
+              </div>
+              <div class="vr"></div>
+              <div class="vstack">
+                <p class="card-text mb-0">Losses:</p>
+                <p attr="accordion-card-2-losses" class="card-text"></p>
+              </div>
+              <div class="vr"></div>
+              <div class="vstack">
+                <p class="card-text mb-0">Win %</p>
+                <p attr="accordion-card-2-win_" class="card-text"></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Third Tab -->
+    <div class="accordion-item">
+      <h2 class="accordion-header">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${i}3" aria-expanded="false" aria-controls="collapse${i}3" tabindex="-1">
+          <div class="hstack gap-3">
+            <i class="fa-solid fa-trophy" style="height: 35px"></i>
+            <p class="m-0">Previous Seasons</p>
+          </div>
+        </button>
+      </h2>
+
+      <div id="collapse${i}3" class="accordion-collapse collapse" data-bs-parent="#accordion${i}">
+        <div class="accordion-body p-0">
+          <div class="card m-0 rounded-0" style="max-width: 740px; min-width: 100px"><div attr="accordion-card-3"></div></div>
+        </div>
+      </div>
+    </div>
+    <!-- Fourth Tab -->
+    <div class="accordion-item">
+      <h2 class="accordion-header">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${i}4" aria-expanded="false" aria-controls="collapse${i}4" tabindex="-1">
+          <div class="hstack gap-3">
+            <i class="fa-solid fa-clock-rotate-left" style="height: 35px"></i>
+            <p class="m-0">Recent Matches</p>
+          </div>
+        </button>
+      </h2>
+
+      <div id="collapse${i}4" class="accordion-collapse collapse" data-bs-parent="#accordion${i}">
+        <div class="accordion-body p-0">
+          <div class="card m-0 rounded-0" style="max-width: 740px; min-width: 100px">
+            <div class="row mx-0 py-1 border-bottom text-info-emphasis">
+              <div class="col"><p class="p-0 m-0">Date</p></div>
+              <div class="col">
+                <p class="p-0 m-0" style="font-size: 0.75em">Result (W,L)</p>
+                <p class="p-0 m-0 fw-bold" style="font-size: 0.75em">K/D</p>
+              </div>
+              <div class="col">
+                <p class="p-0 m-0" style="font-size: 0.75em">RP</p>
+                <p class="p-0 m-0" style="font-size: 0.75em">RP Change</p>
+              </div>
+              <div class="col">
+                <p class="p-0 m-0">Rank</p>
+              </div>
+            </div>
+            <div attr="accordion-card-4"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+  `;
   return data;
 }
+export function platformHandler(playerCol,toggle=false){ // toggle or GET platform
+	let buttonGroup =$(`[player='${playerCol}']`).children('.input-group').children('button')
+	let xbox = buttonGroup.filter('[attr=input-group-button-xbox]')
+	let ps = buttonGroup.filter('[attr=input-group-button-ps]')
+	let platform;
+	if (toggle == true){
+		buttonGroup.each((_,v) => $(v).toggleClass("active"))
+		return;
+	}
+	else if (toggle == false) xbox.hasClass('active') && !ps.hasClass('active') ? platform = "xbox" : platform = "psn";
+	return platform;
+}
+
+
 export async function Fetch(name, platform, player) {
-  platform === 1 ? (platform = "xbox") : (platform = "psn");
-  console.log("ranking", platform, name);
   const a = { playerInfo: [], "trn-cards": [], matchHistory: [], seasons: [] };
 
   if (typeof name !== "string") return "FORMAT_ERROR";
@@ -31,38 +245,12 @@ export async function Fetch(name, platform, player) {
   const data = await exec(url);
   if (!data) return "TIMEOUT";
 
-  //initialize array a
-  a["playerInfo"]["url"] = url;
-
-  // push all profile cards into array a subarray trn-cards
-  const $ = cheerio.load(data);
-  $("#profile .trn-card").each(function (i, elem) {
-    a["trn-cards"].push(filterArray($(this).text().trim().split("\n")));
-  });
-
-  //remove whitespace from all results
-  for (var i = 0; i < a["trn-cards"].length; i++) {
-    var newlength = a["trn-cards"][i].length;
-    for (var j = 0; j < newlength; j++) {
-      a["trn-cards"][i][j] = a["trn-cards"][i][j].trim();
-    }
-  }
+ 
   // push all recent matches into array a subarray matchHistory
   const matchHistory = await exec(matchHistoryUrl);
   const matches = cheerio.load(matchHistory);
 
-  matches(".trn-table__row").each(function (i, elem) {
-    if (i < 21) {
-      a["matchHistory"][i] = filterArray($(this).text().trim().split("\n"));
-      //getting rank pic source from element
-      if (i > 0 && $(this)[0].children[7].children[1] !== undefined) {
-        a["matchHistory"][i].push($(this)[0].children[7].children[1].attribs["src"]);
-        a["matchHistory"][i].push($(this)[0].children[1].children[1].attribs["v-human-time"]);
-        a["matchHistory"][i][7] = a["matchHistory"][i][7].replaceAll(`'`, ``);
-        a["matchHistory"][i].push(humanTime(a["matchHistory"][i][7]));
-      }
-    }
-  });
+  
   // function for converting time into human readable time
   function humanTime(binding) {
     var date = moment.utc(binding);
@@ -85,13 +273,9 @@ export async function Fetch(name, platform, player) {
     a["seasons"][i] = filterArray($(this).text().trim().split("\n"));
   });
   a["seasons"].shift();
-
   return a;
 }
 
-export function LoadDom(d, p) {
-  console.log('trying to load')
-}
 export function LoadSeasonalData(d, p) {
 const addOffsettoSeasonal = (array, value, offset = 1) => (d["card3"][array].indexOf(`${value}`) == -1) ? "N/A" : d["card3"][array][d["card3"][array].indexOf(`${value}`) + offset]
 
@@ -204,7 +388,6 @@ function filterArray(clearArray) {
       result[++resIndex] = value;
     }
   }
-
   return result;
 }
 //Strip alphabetical values from string, return only numbers
@@ -212,21 +395,4 @@ export const StripLetters = (a) => a.replace(/[^\d.-]/g, "") * 1;
 
 export const RemoveWhitespace = (a) => a.replace(/\s/g, "");
 
-
-export function ClearOneAutoComplete(player) {
-  if (player == "all") {
-    for (let i = 0; i <= 5; i++) {
-      $(`#p${i}DropdownClass > li`).remove();
-      $(`#p${i}DropdownClass`).attr("hidden", "");
-    }
-  }
-  $(`#p${player}DropdownClass > li`).remove();
-  $(`#p${player}DropdownClass`).attr("hidden", "");
-}
-
-// platform toggler function
-export function TogglePlatform(player) {
-  $("#x" + player).toggleClass("active");
-  $("#p" + player).toggleClass("active");
-}
 
