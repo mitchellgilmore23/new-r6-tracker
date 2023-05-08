c02843e10106c5bf50e782c0740bffc556657b10
mitchellgilmore23 <117942362+mitchellgilmore23@users.noreply.github.com>
2023-05-08 08:44:34 -0500

    f

--- src/Swipe.js
+++ src/Swipe.js
@@ -1,5 +1,5 @@
 export function addSwipeEvent(theDom, eventName, handleEvent) {
-	var swipeAmt = 300    
+	var swipeAmt = 200
 	var eStart=0, eEnd=0;
 	theDom.addEventListener('touchstart',(e) =>  eStart = e.targetTouches[0].clientX)
 	theDom.addEventListener('mousedown', (e) => eStart = e.clientX)
@@ -7,7 +7,7 @@ export function addSwipeEvent(theDom, eventName, handleEvent) {
 		eEnd = e.changedTouches[0].clientX;
 		var moveVal = eEnd-eStart;
 		var moveAbsVal = Math.abs(moveVal);
-		if (moveVal < 0 && moveAbsVal > swipeAmt && eventName=="swipeLeft")handleEvent();
+		if (moveVal < 0 && moveAbsVal > swipeAmt && eventName=="swipeLeft") handleEvent();
 		if (moveVal>0 && moveAbsVal> swipeAmt && eventName=="swipeRight") handleEvent();
 	});
 	theDom.addEventListener('mouseup', (e) => {
